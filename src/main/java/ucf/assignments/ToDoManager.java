184e65a40ddb9aa72032832eeebfb235e3cc28b3
GitEthan-1 <bulldawg0065@gmail.com>
2021-07-03 14:08:40 -0400

    Renaming and Some Resources added

--- src/main/java/ucf/assignments/ToDoManager.java
+++ src/main/java/ucf/assignments/ToDoManager.java
@@ -17,7 +17,7 @@ public class ToDoManager extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("CatalogLoadGUI.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("LoadOptionsGUI.fxml"));
 
 			Scene scene = new Scene(root);
 
